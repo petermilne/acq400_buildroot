fb8b48101950c980d5e649b5cd460eba3498a5b8
Peter Milne <peter.milne@d-tacq.com>
2019-07-14 21:26:37 +0100

    avoid device missing errors before rcS  On branch cpsc2 	modified:   board/d-tacq/cpsc2/device_table.txt 	new file:   board/d-tacq/cpsc2/device_table_dev.txt 	modified:   board/d-tacq/cpsc2/fakeroot.sh 	modified:   board/d-tacq/cpsc2/rootfs-overlay/etc/fstab 	modified:   board/d-tacq/cpsc2/rootfs-overlay/etc/init.d/rcS 	modified:   configs/cpsc2_main_defconfig

--- board/d-tacq/cpsc2/fakeroot.sh
+++ board/d-tacq/cpsc2/fakeroot.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/bash
 
-
-echo FAKEROOT01 promote libtirpc to /lib .. needed by busybox
-echo FAKEROOT01 before $(ls -l output/target/dev/null)
-echo FAKEROOT01 mknod -m 666 output/target/dev/null c 1 3
-mknod -m 666 output/target/dev/null c 1 3
-echo FAKEROOT66 after  $(ls -l output/target/dev/null)
-#mv output/target/usr/lib/libtirpc.so* output/target/lib
+echo FAKEROOT01
 echo FAKEROOT99
