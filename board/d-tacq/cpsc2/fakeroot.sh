dbfaee8733599e8a0b0c1b16e6ed034935ae7aed
Peter Milne <peter.milne@d-tacq.com>
2019-07-14 15:35:19 +0100

    create /dev/null  On branch cpsc2 	modified:   board/d-tacq/cpsc2/fakeroot.sh

--- board/d-tacq/cpsc2/fakeroot.sh
+++ board/d-tacq/cpsc2/fakeroot.sh
@@ -2,5 +2,9 @@
 
 
 echo FAKEROOT01 promote libtirpc to /lib .. needed by busybox
+echo FAKEROOT01 before $(ls -l output/target/dev/null)
+echo FAKEROOT01 mknod -m 666 output/target/dev/null c 1 3
+mknod -m 666 output/target/dev/null c 1 3
+echo FAKEROOT66 after  $(ls -l output/target/dev/null)
 #mv output/target/usr/lib/libtirpc.so* output/target/lib
 echo FAKEROOT99
