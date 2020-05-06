4afdac6b32ff4f3c54e3a6749ec761e80bc75976
Peter Milne <peter.milne@d-tacq.com>
2020-05-06 10:53:37 +0100

    postbuild created /etc/acq400_version  On branch acq400_v2020.02 	modified:   board/d-tacq/acq400/postbuild.sh

--- board/d-tacq/acq400/postbuild.sh
+++ board/d-tacq/acq400/postbuild.sh
@@ -2,5 +2,6 @@
 
 
 echo POSTBUILD01 
-
+echo "acq400_buildroot $(git describe --abbrev=0) ${USER}@$(hostname) $(date) $(git show --quiet --pretty=format:%H)" \
+	> output/target/etc/acq400_version
 echo POSTBUILD99
