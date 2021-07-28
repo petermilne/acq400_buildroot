bfd76e021dc43597906de933d706ced5e02dccbb
Peter Milne <peter.milne@d-tacq.com>
2021-07-28 16:00:30 +0100

    pyepics: somewhere, the linuxarm clibs get stripped and fail. we fix this in fakeroot.sh, but BEWARE, it will be brittle  On branch acq400_v2021.05 	modified:   board/d-tacq/acq400/README 	modified:   board/d-tacq/acq400/fakeroot.sh 	modified:   package/python-pyepics/python-pyepics.hash 	modified:   package/python-pyepics/python-pyepics.mk

--- board/d-tacq/acq400/fakeroot.sh
+++ board/d-tacq/acq400/fakeroot.sh
@@ -18,4 +18,19 @@ cp output/build/ntp-4.2.8p13/ntpd/ntpd output/target/usr/sbin/ntpd.orig
 echo AFTER NTPD FIXUP
 ls -l output/target/usr/sbin/ntpd*
 
+echo BEFORE PYEPICS FIXUP
+du output/target/usr/lib/python*/site-packages/epics/clibs
+rm -Rf output/target/usr/lib/python*/site-packages/epics/clibs/*
+if [ ! -e output/target/usr/lib/python3.9/site-packages/epics/clibs ]; then
+	echo HOUSTON, we have a problem: output/target/usr/lib/python3.9/site-packages/epics/clibs does not exist
+	exit 1
+fi
+if [ ! -e output/build/python-pyepics-3.5.0/epics/clibs/linuxarm ]; then
+	echo HOUSTON, we have a problem: output/build/python-pyepics-3.5.0/epics/clibs/linuxarm does not exist
+	exit 1
+fi
+mkdir output/target/usr/lib/python3.9/site-packages/epics/clibs/linuxarm
+cp -R output/build/python-pyepics-3.5.0/epics/clibs/linuxarm/*  output/target/usr/lib/python3.9/site-packages/epics/clibs/linuxarm
+echo AFTER PYEPICS FIXUP
+du output/target/usr/lib/python*/site-packages/epics/clibs
 echo FAKEROOT99
--- package/python-pyepics/python-pyepics.mk
+++ package/python-pyepics/python-pyepics.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-PYTHON_PYEPICS_VERSION = 3.4.1
+PYTHON_PYEPICS_VERSION = 3.5.0
 PYTHON_PYEPICS_SOURCE = pyepics-$(PYTHON_PYEPICS_VERSION).tar.gz
-PYTHON_PYEPICS_SITE = https://files.pythonhosted.org/packages/cb/64/f849f7aef93b7d3d713aa1d5e79ee4b67c84fa3fa1db3e2882e91c000ff6
+PYTHON_PYEPICS_SITE = https://files.pythonhosted.org/packages/22/f2/3b15bc9b2d148a19686d5de8901a55f2bbd0a67a63681a0c1ef12c998fa2
 PYTHON_PYEPICS_SETUP_TYPE = setuptools
 PYTHON_PYEPICS_LICENSE = Epics Open License
 PYTHON_PYEPICS_LICENSE_FILES = LICENSE
