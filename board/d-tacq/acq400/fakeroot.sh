8e0648d007caaa117200d91c47a086e2cd66e877
Peter Milne <peter.milne@d-tacq.com>
2021-08-10 14:35:05 +0100

    autodetect ntp version to get the actual version.. .. are there other fixups that could use this hack?  On branch acq400_v2021.05 	modified:   fakeroot.sh

--- board/d-tacq/acq400/fakeroot.sh
+++ board/d-tacq/acq400/fakeroot.sh
@@ -14,7 +14,10 @@ ls -l output/target/usr/sbin/nologin
 ls -l output/target/usr/sbin/ntpd
 rm -f output/target/usr/sbin/ntpd
 (cd output/target/usr/sbin/; ln -s ../../bin/busybox ntpd)
-cp output/build/ntp-4.2.8p13/ntpd/ntpd output/target/usr/sbin/ntpd.orig
+NTP=$(basename output/build/ntp-*/)
+NTP_VERSION=${NTP#*-}
+echo Hello NTP_VERSION $NTP_VERSION
+cp output/build/ntp-$NTP_VERSION/ntpd/ntpd output/target/usr/sbin/ntpd.orig
 echo AFTER NTPD FIXUP
 ls -l output/target/usr/sbin/ntpd*
 
