b3d67b3c769ddec7bdfa4e55c9eed88da88f8f70
Peter Milne <peter.milne@d-tacq.com>
2022-03-12 12:41:12 +0000

    clean up detecting hostname  On branch acq400_v2022.03 	modified:   postbuild.sh 	modified:   rootfs-overlay/etc/init.d/rcS

--- board/d-tacq/acq400/postbuild.sh
+++ board/d-tacq/acq400/postbuild.sh
@@ -10,6 +10,7 @@ else
 fi
 echo "acq400_buildroot $(git describe --abbrev=0) ${USER}@$(hostname) $(date) $(git show --quiet --pretty=format:%H)" $GITSTAT \
 	> output/target/etc/acq400_version
+sed -ie '/hostname/d' output/target/etc/inittab
 rm output/target/etc/hostname
 echo POSTBUILD99
 echo ----------------------------------------------------------------------
