4a52c895241a602cc78589a44ca391e09d882068
Peter Milne <peter.milne@d-tacq.com>
2023-04-04 17:52:07 +0100

    +ACQ400_UTIL_VERSION 1.4 : install all to /usr/bin  On branch acq400 	modified:   package/acq400_util/acq400_util.mk

--- package/acq400_util/acq400_util.mk
+++ package/acq400_util/acq400_util.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ACQ400_UTIL_VERSION = 1.2
+ACQ400_UTIL_VERSION = 1.4
 ACQ400_UTIL_SITE = $(call github,petermilne,acq400_utils,V$(ACQ400_UTIL_VERSION))
 ACQ400_UTIL_INSTALL_STAGING = YES
 
@@ -12,16 +12,21 @@ ifeq ($(BR2_PACKAGE_POPT),y)
     ACQ400_UTIL_DEPENDENCIES += popt
 endif
 
+ifeq ($(BR2_PACKAGE_TINYXML),y)
+    ACQ400_UTIL_DEPENDENCIES += tinyxml
+endif
+
 define ACQ400_UTIL_BUILD_CMDS
 	$(MAKE) CC="$(TARGET_CC)" CXX="$(TARGET_CXX)" LD="$(TARGET_LD)" -C $(@D) all
 endef
 
 define ACQ400_UTIL_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/fs2xml $(TARGET_DIR)/bin
-	$(INSTALL) -D -m 0755 $(@D)/ob_calc_527 $(TARGET_DIR)/bin
-	$(INSTALL) -D -m 0755 $(@D)/daemon $(TARGET_DIR)/bin
-	$(INSTALL) -D -m 0755 $(@D)/xiloader $(TARGET_DIR)/bin
-	$(INSTALL) -D -m 0755 $(@D)/dump-cstrings $(TARGET_DIR)/bin
+	$(INSTALL) -D -m 0755 $(@D)/fs2xml $(TARGET_DIR)/usr/bin
+	$(INSTALL) -D -m 0755 $(@D)/ob_calc_527 $(TARGET_DIR)/usr/bin
+	$(INSTALL) -D -m 0755 $(@D)/daemon $(TARGET_DIR)/usr/bin
+	$(INSTALL) -D -m 0755 $(@D)/xiloader $(TARGET_DIR)/usr/bin
+	$(INSTALL) -D -m 0755 $(@D)/dump-cstrings $(TARGET_DIR)/usr/bin
+	$(INSTALL) -D -m 0755 $(@D)/xml_lookup $(TARGET_DIR)/usr/bin
 endef
 
 $(eval $(generic-package))
